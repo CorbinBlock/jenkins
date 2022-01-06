655528e5754f2fe3c8ffdd3472aa6977d75c1643
root <root@CBLOCKDYFX2X2.pclc0.merkle.local>
2022-01-05 20:58:25 -0600

    initial

--- groovy_plugin_test/execute_shell.groovy
+++ groovy_plugin_test/execute_shell.groovy
@@ -3,14 +3,13 @@ String shell_name = "/bin/bash"
 String option_name = "-c"
 
 String shell_command ="""
-python --version
-java --version
-groovy --version
-
+ls -ltr
 """
 
 String shell_script ="""
 ${shell_name} ${option_name} ${shell_command}
 """
 
-shell_script.execute().text
+def p = shell_script.execute()
+p.waitFor()
+print(p.text)
